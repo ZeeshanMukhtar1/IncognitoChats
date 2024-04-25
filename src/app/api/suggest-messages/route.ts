ef9b2c87a2167b02a9fb7b5e175a710adb4fc108
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-04-25 22:17:26 +0500

    integrated openai SDK

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,41 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
+
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const dynamic = 'force-dynamic';
+
+export async function POST(req: Request) {
+  try {
+    // const { messages } = await req.json();
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    // Ask OpenAI for a streaming completion given the prompt
+    const response = await openai.completions.create({
+      model: 'gpt-3.5-turbo-instruct',
+      max_tokens: 400,
+      stream: true,
+      prompt,
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      const { name, message, status, headers } = error;
+      return NextResponse.json({ name, message, status, headers }, { status });
+    } else {
+      console.error('Error in suggest-messages route: ', error);
+      throw error;
+    }
+  }
+}
+
+// docs link : https://sdk.vercel.ai/docs/guides/providers/openai
